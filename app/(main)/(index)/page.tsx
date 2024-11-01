04f56a0fcf515c5515decaddecd68007481141f0
Tianyun Zhang <doowzs@qq.com>
2024-11-01 21:02:59 +0800

    fix displaying title on index page

--- app/(main)/(index)/page.tsx
+++ app/(main)/(index)/page.tsx
@@ -1,9 +1,15 @@
+import { config } from "@/lib/config";
+import { Metadata } from "next";
 import { IndexBulletin } from "./bulletin";
+
+export const metadata: Metadata = {
+  title: `系统首页 | ${config.siteName}`,
+};
 
 export default function IndexPage() {
   return (
-    <div>
+    <>
       <IndexBulletin />
-    </div>
+    </>
   );
 }
